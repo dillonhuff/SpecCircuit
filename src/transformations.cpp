fe966307cf602b4aa80df03f3a81230c5200efc6
Dillon Huff <dillonhuff@gmail.com>
2018-04-22 18:04:38 -0700

    Need to work on mux rewiring

--- src/transformations.cpp
+++ src/transformations.cpp
@@ -10,8 +10,34 @@ namespace FlatCircuit {
     return {};
   }
 
-  // NOTE: This structure will not quite work for muxes. Need to come up
-  // with a slightly different structure for them.
+  maybe<BitVector> getInput(Cell& cell,
+                            const PortId pid,
+                            CellDefinition& def) {
+    BitVector bv(cell.getPortWidth(pid), 0);
+
+    auto& drivers = cell.getDrivers(pid);
+    for (int offset = 0; offset < drivers.signals.size(); offset++) {
+      SignalBit driver = drivers.signals.at(offset);
+      if (notEmpty(driver)) {
+        CellType driverTp = def.getCellRef(driver.cell).getCellType();
+        if (driverTp != CELL_TYPE_CONST) {
+          return {};
+        }
+
+        assert(driver.port == PORT_ID_OUT);
+
+        Cell& driverCell = def.getCellRef(driver.cell);
+        BitVector driverValue = driverCell.getParameterValue(PARAM_INIT_VALUE);
+
+        bv.set(offset, driverValue.get(offset));
+      } else {
+        return {};
+      }
+    }
+
+    return bv;
+  }
+
   void foldConstants(CellDefinition& def) {
     set<CellId> candidates;
     for (auto cellPair : def.getCellMap()) {
@@ -33,17 +59,26 @@ namespace FlatCircuit {
 
       Cell& nextCell = def.getCellRef(next);
 
-      maybe<BitVector> bv = getOutput(nextCell, def);
+      if (nextCell.getCellType() == CELL_TYPE_MUX) {
+        maybe<BitVector> bv = getInput(nextCell, PORT_ID_SEL, def);
 
-      if (bv.has_value()) {
-        for (auto sigBus : nextCell.getPortReceivers(PORT_ID_OUT)) {
-          for (auto sigBit : sigBus) {
-            candidates.insert(sigBit.cell);
-          }
+        if (bv.has_value()) {
+          // TODO: Add rewiring for muxes with constant value selects
+          assert(false);
         }
+      } else {
+        maybe<BitVector> bv = getOutput(nextCell, def);
 
-        def.replaceCellPortWithConstant(next, PORT_ID_OUT, bv.get_value());
+        if (bv.has_value()) {
+          for (auto sigBus : nextCell.getPortReceivers(PORT_ID_OUT)) {
+            for (auto sigBit : sigBus) {
+              candidates.insert(sigBit.cell);
+            }
+          }
+
+          def.replaceCellPortWithConstant(next, PORT_ID_OUT, bv.get_value());
         
+        }
       }
     }
 
