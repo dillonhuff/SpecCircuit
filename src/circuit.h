acba5674741679853eaddfe8e5f0bf28a380555b
Dillon Huff <dillonhuff@gmail.com>
2018-04-22 17:44:02 -0700

    Now need to build up cell outputs

--- src/circuit.h
+++ src/circuit.h
@@ -723,35 +723,12 @@ namespace FlatCircuit {
 
     void replacePortWithConstant(const std::string& portName,
                                  const BitVector& constValue) {
-      // CellId constId = addCell(portName + "_const_replacement",
-      //                          CELL_TYPE_CONST,
-      //                          {{PARAM_WIDTH, BitVector(32, constValue.bitLength())},
-      //                              {PARAM_INIT_VALUE, constValue}});
-
-      //Cell& constCell = getCellRef(constId);
-
       CellId portCellId = getPortCellId(portName);
 
       replaceCellPortWithConstant(portCellId,
                                   PORT_ID_OUT,
                                   constValue);
       
-      //Cell& portCell = getCellRef(portId);
-
-      
-      // std::vector<std::vector<SignalBit> > receivers =
-      //   portCell.getPortReceivers(PORT_ID_OUT);
-      
-      // for (int offset = 0; offset < receivers.size(); offset++) {
-      //   auto& sigBus = receivers.at(offset);
-      //   SignalBit driverSignal{constId, PORT_ID_OUT, offset};
-      //   for (auto receiverSignal : sigBus) {
-      //     setDriver(receiverSignal, driverSignal);
-      //   }
-      // }
-
-      // // NOTE: Drivers and receivers are not kept in sync here
-      // portCell.clearReceivers(PORT_ID_OUT);
     }
 
     void replaceCellPortWithConstant(const CellId cid,
--- src/transformations.cpp
+++ src/transformations.cpp
@@ -6,15 +6,26 @@ namespace FlatCircuit {
 
   maybe<BitVector> getOutput(Cell& nextCell,
                              CellDefinition& def) {
+    
     return {};
   }
 
   void foldConstants(CellDefinition& def) {
     set<CellId> candidates;
-    
+    for (auto cellPair : def.getCellMap()) {
+      if (cellPair.first == CELL_TYPE_CONST) {
+        for (auto sigBus : cellPair.second.getPortReceivers(PORT_ID_OUT)) {
+          for (auto sigBit : sigBus) {
+            candidates.insert(sigBit.cell);
+          }
+        }
+      }
+    }
 
     while (candidates.size() > 0) {
       CellId next = *std::begin(candidates);
+      candidates.erase(next);
+
       Cell& nextCell = def.getCellRef(next);
 
       maybe<BitVector> bv = getOutput(nextCell, def);
