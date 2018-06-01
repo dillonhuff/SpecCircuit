c6b2b494d51b80a968277f9a6ab63f5beb3ae3dc
Dillon Huff <dillonhuff@gmail.com>
2018-06-01 15:16:13 -0700

    Adding automated testing material

--- benchmarks/create_tb.py
+++ benchmarks/create_tb.py
@@ -1,3 +1,4 @@
+import os
 
 # config_file_name = './test/conv_3_1_only_config_lines.bsa'
 # output_file_name = 'conv_3_1_tb_output.txt'
@@ -24,15 +25,37 @@ def generate_tb_from_template(tb_template_file, config_file_name, output_file_na
     tb_output_file.write(new_tb)
     tb_output_file.close()
 
-def generate_tb_for_application_from_template(app_name, tb_template_file):
+def generate_tb_for_application_from_template(app_name, bitstream_name, tb_template_file):
     # Generate the real test benches
-    config_file_name = './test/' + app_name + '_only_config_lines.bsa'
+    config_file_name = './test/' + bitstream_name + '_only_config_lines.bsa'
     output_file_name = app_name + '_tb_output.txt'
     tb_name = app_name + '_tb.v'
     tb_template_file = './benchmarks/test.v'
 
     generate_tb_from_template(tb_template_file, config_file_name, output_file_name, tb_name)
 
-generate_tb_for_application_from_template('conv_3_1', './benchmarks/test.v')
-generate_tb_for_application_from_template('conv_2_1', './benchmarks/test.v')
-generate_tb_for_application_from_template('conv_bw', './benchmarks/test.v')
+def run_iverilog(app_name, tb_name, top_mod_file_name):
+    compile_cmd = 'iverilog -o ' + app_name + ' ' + tb_name + ' ' + top_mod_file_name
+    print 'iverilog compile command = ', compile_cmd
+    compile_res = os.system(compile_cmd)
+
+    assert(compile_res == 0)
+
+    run_cmd = './' + app_name
+    print 'iverilog run command = ', run_cmd
+    run_res = os.system(run_cmd)
+
+    assert(run_res == 0)
+
+generate_tb_for_application_from_template('conv_3_1_specialized', 'conv_3_1', './benchmarks/test.v')
+run_iverilog('conv_3_1_specialized', 'conv_3_1_specialized_tb.v', 'conv_3_1_cgra.v')
+
+generate_tb_for_application_from_template('conv_2_1_specialized', 'conv_2_1', './benchmarks/test.v')
+run_iverilog('conv_2_1_specialized', 'conv_2_1_specialized_tb.v', 'conv_2_1_cgra.v')
+
+generate_tb_for_application_from_template('conv_bw_specialized', 'conv_bw', './benchmarks/test.v')
+run_iverilog('conv_bw_specialized', 'conv_bw_specialized_tb.v', 'conv_bw_cgra.v')
+
+# Q: What should the testbench structure be?
+# A: I think that I want it to be: A specialized version and an unspecialized version
+#    of each testbench, so the file name needs to be different
