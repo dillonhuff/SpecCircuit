d73762f8244461f4dfe215d51c6eec6e3211a873
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-06-09 02:47:54 -0700

    Now have somewhat coherent printouts

--- benchmarks/process_tb_times.py
+++ benchmarks/process_tb_times.py
@@ -1,13 +1,66 @@
-myfile = open('conv_3_1_unspecialized_tb_metadata.txt', 'r')
-file_str = myfile.read()
-myfile.close()
+def seconds_from_date(date_str):
+    hr_min_sec = date_str.partition(":")
 
-ls = file_str.splitlines()
+    # print 'hr_min_sec comps'
+    # for cp in hr_min_sec:
+    #     print cp
+        
+    hour = int(hr_min_sec[0])
 
-for line in ls:
-    print line
+    min_sec = hr_min_sec[2].partition(":")
+    minute = int(min_sec[0])
+    sec = int(min_sec[2])
 
-assert(len(ls) == 2)
+    return hour*3600 + minute*60 + sec
 
-start_time_str = ls[0]
-end_time_str = ls[1]
+def process_specialized_line(file_str):
+    ls = file_str.splitlines()
+
+    start_time_str = ls[0]
+    end_time_str = ls[1]
+
+    s_str = 'Start='
+    e_str = 'Finish='
+
+    assert(start_time_str.startswith(s_str))
+    assert(end_time_str.startswith(e_str))
+
+    start_time_str = start_time_str[len(s_str):]
+    end_time_str = end_time_str[len(e_str):]
+
+    start_date_str = start_time_str.partition("--")[2]
+    end_date_str = end_time_str.partition("--")[2]
+
+    assert(start_date_str == end_date_str)
+
+    start_time_str = start_time_str.partition("--")[0]
+    end_time_str = end_time_str.partition("--")[0]
+
+    start_seconds = seconds_from_date(start_time_str)
+    end_seconds = seconds_from_date(end_time_str)
+    
+    assert((end_seconds - start_seconds) >= 0)
+
+    print 'Start seconds           = ', start_seconds
+    print 'End seconds             = ', end_seconds
+    print 'Runtime Difference      = ', (end_seconds - start_seconds)
+    
+def process_metadata_file(file_name):
+    myfile = open(file_name, 'r')
+    file_str = myfile.read()
+    myfile.close()
+
+    process_specialized_line(file_str)
+
+def process_metadata_files(app_name):
+    unspecialized_file_name = app_name + '_unspecialized_tb_metadata.txt'
+    print '----- Timing for ', unspecialized_file_name
+    process_metadata_file(unspecialized_file_name)
+
+    specialized_file_name = app_name + '_specialized_tb_metadata.txt'
+    print '----- Timing for ', specialized_file_name
+    process_metadata_file(specialized_file_name)
+    
+process_metadata_files('conv_3_1')
+process_metadata_files('conv_2_1')
+process_metadata_files('conv_bw_travis')
