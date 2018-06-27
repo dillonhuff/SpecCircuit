613337d20761bdf605427e93520934487018f6b0
Dillon Huff <dillonhuff@gmail.com>
2018-06-27 14:27:37 -0700

    Pre specialized conv does not pass tests?

--- test/op_tests.cpp
+++ test/op_tests.cpp
@@ -4,6 +4,28 @@
 
 namespace FlatCircuit {
 
+  CellDefinition& buildReduceBinopCellDef(Env& e, CellType opType) {
+    CellType notChainType = e.addCellType(toString(opType) + "_test_cell");
+    CellDefinition& def = e.getDef(notChainType);
+    def.addPort("in0", 16, PORT_TYPE_IN);
+    def.addPort("in1", 16, PORT_TYPE_IN);
+    def.addPort("out", 1, PORT_TYPE_OUT);
+
+    CellId binop =
+      def.addCell(toString(opType) + "_test_inst",
+                  opType,
+                  {{PARAM_WIDTH, BitVector(32, 16)}});
+
+    CellId in0Cell = def.getPortCellId("in0");
+    CellId in1Cell = def.getPortCellId("in1");
+    CellId outCell = def.getPortCellId("out");
+    def.connect(in0Cell, PORT_ID_OUT, binop, PORT_ID_IN0);
+    def.connect(in1Cell, PORT_ID_OUT, binop, PORT_ID_IN1);
+    def.connect(binop, PORT_ID_OUT, outCell, PORT_ID_IN);
+
+    return def;
+  }
+
   CellDefinition& buildBinopCellDef(Env& e, CellType opType) {
     CellType notChainType = e.addCellType(toString(opType) + "_test_cell");
     CellDefinition& def = e.getDef(notChainType);
@@ -26,6 +48,35 @@ namespace FlatCircuit {
     return def;
   }
 
+  void compareSimulators(Simulator& interpSim, Simulator& compileSim) {
+    for (int i = 0; i < 200; i++) {
+
+      int in0V = min + (rand() % static_cast<int>(max - min + 1));
+      int in1V = min + (rand() % static_cast<int>(max - min + 1));
+      BitVector in0 = BitVector(16, in0V);
+      BitVector in1 = BitVector(16, in1V);
+
+      cout << "in0 = " << in0 << ", " << in0.to_type<int>() << endl;
+      cout << "in1 = " << in1 << ", " << in1.to_type<int>() << endl;
+
+      interpSim.setFreshValue("in0", in0);
+      interpSim.setFreshValue("in1", in1);
+      interpSim.update();
+
+      compileSim.setFreshValue("in0", in0);
+      compileSim.setFreshValue("in1", in1);
+      compileSim.update();
+
+      auto interpOut = interpSim.getBitVec("out");
+      auto compiledOut = compileSim.getBitVec("out");
+        
+      cout << "interp out    = " << interpOut << ", " << interpOut.to_type<int>() << endl;
+      cout << "compiled out  = " << compiledOut << ", " << compiledOut.to_type<int>() << endl;
+        
+      REQUIRE(interpSim.getBitVec("out") == compileSim.getBitVec("out"));
+    }
+  }
+
   TEST_CASE("Comparing binary ops in simulation and interpretation") {
     Env e;
     int max = 1 << 15;
@@ -44,33 +95,7 @@ namespace FlatCircuit {
       Simulator compileSim(e, def);
       compileSim.compileCircuit();
 
-
-      for (int i = 0; i < 200; i++) {
-
-        int in0V = min + (rand() % static_cast<int>(max - min + 1));
-        int in1V = min + (rand() % static_cast<int>(max - min + 1));
-        BitVector in0 = BitVector(16, in0V);
-        BitVector in1 = BitVector(16, in1V);
-
-        cout << "in0 = " << in0 << ", " << in0.to_type<int>() << endl;
-        cout << "in1 = " << in1 << ", " << in1.to_type<int>() << endl;
-
-        interpSim.setFreshValue("in0", in0);
-        interpSim.setFreshValue("in1", in1);
-        interpSim.update();
-
-        compileSim.setFreshValue("in0", in0);
-        compileSim.setFreshValue("in1", in1);
-        compileSim.update();
-
-        auto interpOut = interpSim.getBitVec("out");
-        auto compiledOut = compileSim.getBitVec("out");
-        
-        cout << "interp out    = " << interpOut << ", " << interpOut.to_type<int>() << endl;
-        cout << "compiled out  = " << compiledOut << ", " << compiledOut.to_type<int>() << endl;
-        
-        REQUIRE(interpSim.getBitVec("out") == compileSim.getBitVec("out"));
-      }
+      compareSimulators(interpSim, compileSim);
     }
 
   }
