c212f72267a6e2f03c7d3296f6b13bce83c109e0
Dillon Bailey Huff <dhuff@kiwi.Stanford.EDU>
2018-07-10 19:56:54 -0700

    Changes to x_prop_test

--- benchmarks/x_prop_test.v
+++ benchmarks/x_prop_test.v
@@ -4,17 +4,17 @@ module test();
    wire [15:0] a;
    wire [15:0] b;
 
-   wire [15:0] a_lt_b;
+   wire [0:0] a_lt_b;
 
    reg [31:0]  cycle_count;
    reg [31:0] cycle_max;
    
    
 
-   assign a = 16'b100001000000000;
-   assign b = 16'b0000x1000000000;
+   assign a = 16'b1100_0000_0000_0000;
+   assign b = 16'b0000_0x00_0000_0000;
 
-   assign a_lt_b = a + b;
+   assign a_lt_b = a <= b;
    
 
    initial begin
